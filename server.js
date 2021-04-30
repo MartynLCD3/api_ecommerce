6c4eb7083b1f2f165d43ee8051e2bf5c39e193c6
MartynLCD3 <martincastagno@protonmail.com>
2021-04-30 09:42:50 -0300

    Configuracion CORS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import {connection} from './config/connection.js';
 import {add_product} from './controllers/add_product.js';
 import {get_products} from './controllers/get_products.js';
@@ -9,27 +10,28 @@ const app = express();
 const router = express.Router();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cors());
 
-router.get('/',(req,res) => {
+router.get('/',cors(),(req,res) => {
     res.status(200).json({msg:"welcome to the ecommerce API"});
 });
 
-router.get('/products',(req,res) => {
+router.get('/products',cors(),(req,res) => {
     Promise.all([get_products()]).then(objects => { 
         let [products] = objects;
         res.status(200).json(products);
     }) 
 });
 
-router.post('/new_product',(req,res) => {
+router.post('/new_product',cors(),(req,res) => {
     add_product(req.body,res);
 });
 
-router.put('/:id',(req,res) => {
+router.put('/:id',cors(),(req,res) => {
     upd_product(req,res);
 });
 
-router.delete('/:id',(req,res) => {
+router.delete('/:id',cors(),(req,res) => {
     del_product(req,res);
 });
 
